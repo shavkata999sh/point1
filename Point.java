51f9ace46c6cc2f26495fa41e27a5fc9815819da
Shavkat <156001276+shavkata999sh@users.noreply.github.com>
2024-01-20 18:20:52 +0500

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,14 @@
+public class Point {
+    int x=10;
+    int y=20;
+    int z=30;
+    public static void main(String []args){
+        Point main = new Point();
+        main.x=10;
+        main.y=20;
+        main.z=30;
+        System.out.println(main.x);
+        System.out.println(main.y);
+        System.out.println(main.z);
+    }
+}
